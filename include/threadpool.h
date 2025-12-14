2bd43638a4b1b49a250b0a491990b91e06c55789
saikiran <saikiranrajupeta@gmail.com>
2025-12-14 18:47:12 +0530

    Update threadpool.h

--- include/threadpool.h
+++ include/threadpool.h
@@ -3,13 +3,28 @@
 
 #include <pthread.h>
 
-typedef struct {
+/* Opaque task structure (internal use) */
+typedef struct task task_t;
+
+/* Thread pool structure */
+typedef struct threadpool {
     pthread_t *threads;
-    int num_threads;
-    int stop;
+
+    int thread_count;
+    int shutdown;
+
+    task_t *task_head;
+    task_t *task_tail;
+
+    pthread_mutex_t lock;
+    pthread_cond_t task_available;
 } threadpool_t;
 
-int threadpool_init(threadpool_t *pool, int num_threads);
+/* Public API */
+void threadpool_init(threadpool_t *pool, int num_threads);
+void threadpool_submit(threadpool_t *pool,
+                       void (*function)(void *),
+                       void *argument);
 void threadpool_destroy(threadpool_t *pool);
 
 #endif
